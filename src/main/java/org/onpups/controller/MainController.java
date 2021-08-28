a16873b2c47619d5695580ea7fb5cc33025e2441
dbdom <dbdom@DESKTOP-5CTD76L>
2021-08-28 10:19:44 +0900

    notebook page create

--- src/main/java/org/onpups/controller/MainController.java
+++ src/main/java/org/onpups/controller/MainController.java
@@ -19,7 +19,7 @@ import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j
-@RequestMapping("/main")
+@RequestMapping("/main/")
 @RequiredArgsConstructor     // 이게있어야 서비스 final로 선언시 impl 과 연결시켜 자동매핑 
 public class MainController {
 	private final SampleService sampleService;
